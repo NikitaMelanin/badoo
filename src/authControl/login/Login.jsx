25ab150ffe787a211b7372b16aa279a69eb5e1a5
gperman <g.perman@artsofte.ru>
2022-11-09 14:28:58 +0500

    add info

--- src/authControl/login/Login.jsx
+++ src/authControl/login/Login.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 export function Login() {
+    const eventFunc = () => {
+
+        fetch("http://public.loc/api/login_check", {
+            method: "POST",
+            body: JSON.stringify({"username": loginFormUsername.current.value, "password": loginFormPassword.current.value})
+        })
+            .then((response) => (response.json()))
+            .then((response) => {
+                localStorage.setItem(response.token);
+            })
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    const loginFormUsername = useRef(null);
+    const loginFormPassword = useRef(null);
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter username" />
+                    <Form.Control type="text" placeholder="Enter username" ref={loginFormUsername} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" ref={loginFormPassword} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" onClick={eventFunc}>
                     Submit
                 </Button>
             </Form>
